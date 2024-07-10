fe09c376179943a5c1d04cf3a35f96adfd2e5940
Mihir Khandekar <mihirkhandekar@gmail.com>
2024-07-10 10:43:55 +0800

    Add t&c

--- who-why-when-landing-page/src/components/Footer.svelte
+++ who-why-when-landing-page/src/components/Footer.svelte
@@ -1,15 +1,18 @@
 <footer class="footer">
-    <div class="container">
+  <div class="container">
       <p>&copy; 2024 WhoWhyWhen. All rights reserved.</p>
-    </div>
-  </footer>
-  
-  <style>
-    .footer {
+      <p>
+          <a href="/privacy-policy.html" style="color: #fff; text-decoration: underline;">Privacy Policy</a> | 
+          <a href="/terms-conditions.html" style="color: #fff; text-decoration: underline;">Terms and Conditions</a>
+      </p>
+  </div>
+</footer>
+
+<style>
+  .footer {
       background: #663399;
       color: #fff;
       padding: 20px 0;
       text-align: center;
-    }
-  </style>
-  
+  }
+</style>
