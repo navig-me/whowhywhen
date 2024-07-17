89542c5bcd9ae8c2ff7930eab2733317b97df7b3
Mihir Khandekar <mihirkhandekar@gmail.com>
2024-07-17 14:14:27 +0800

    Update docker commands

--- update_stack.sh
+++ update_stack.sh
@@ -1,9 +1,18 @@
+# Pull the latest changes from the Git repository
 git pull
+
+# Build the Docker images
 docker-compose build
 
+# Remove the existing services from the stack
 docker service rm www_stack_dash
 docker service rm www_stack_api
 docker service rm www_stack_svelte
 docker service rm www_stack_traefik
+docker service rm www_stack_celery
+docker service rm www_stack_celery_beat
+docker service rm www_stack_redis
+docker service rm www_stack_flower
 
+# Deploy the updated stack
 docker stack deploy -c docker-compose.yml www_stack
